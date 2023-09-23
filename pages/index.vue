9c28b1082f32e6c6f2189737ac22746a360c7d46
BenoitEirik <olafsergent@gmail.com>
2023-09-23 17:35:25 +0200

    Configuration

---
+++ pages/index.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="flex justify-center items-center">
+  <div>
+    <ContentDoc path="/blog/hello"></ContentDoc>
+  </div>
+</div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  script: [
+    { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" },
+  ]
+})
+</script>
